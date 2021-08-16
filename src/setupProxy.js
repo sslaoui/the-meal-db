29f5c4599062d7279b38ca157feb5e96b0856fdd
Saad Slaoui <saad.slaoui@man.eu>
2021-08-16 23:05:16 +0200

    fixing backend call

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,15 +2,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        createProxyMiddleware('/categories', {
-            target: 'http://themealdb.com/api/json/v2/9973533/',
+        createProxyMiddleware('/categories.php', {
+            target: 'https://themealdb.com/api/json/v2/9973533',
             secure: false,
             changeOrigin: true,
         })
     );
     app.use(
-        createProxyMiddleware('/latest', {
-            target: 'http://themealdb.com/api/json/v2/9973533/',
+        createProxyMiddleware('/latest.php', {
+            target: 'https://themealdb.com/api/json/v2/9973533',
             secure: false,
             changeOrigin: true,
         })
